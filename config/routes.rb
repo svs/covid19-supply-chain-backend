2f8ed58ecb2411c19f689297370e4b129ca033fb
Shivam Rastogi <shivamsupr@gmail.com>
2020-03-26 15:57:17 +0530

    react integration with rails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root 'pages#index'
+  # root 'pages#index'
+  
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
