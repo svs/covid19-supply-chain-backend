fb5f6d1b75a34f58508314cddbb24aff8c0fcb6b
Swapnil Singh <mail@swapnilsingh.me>
2020-04-03 15:45:15 +0530

    Adds discord link

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,14 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Layout } from 'antd'
+import { Layout, Typography, Button } from 'antd'
+import { DiscordIcon } from './Icons'
 import './Footer.css'
 
 const { Footer } = Layout
+const { Text } = Typography
 
 const FooterComp = ({ children }) => {
     return (
         <Footer className="Footer">
-            Made with clean hands 🙌 working from home 🏠
+            <Button type="link" href="https://discord.gg/j7Wjg7c">
+                <DiscordIcon style={{ width: '16px', height: '16px' }} />Join Discord Chat
+            </Button>
+            <Text>Made with clean hands 🙌 working from home 🏠</Text>
         </Footer>
     );
 }
--- app/javascript/components/Intro.js
+++ app/javascript/components/Intro.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Carousel, Space, Typography } from 'antd'
 import './Intro.css';
 
-const { Text } = Typography;
+const { Text, Paragraph } = Typography;
 
 const Intro = () => {
     return (
@@ -16,16 +16,20 @@ const Intro = () => {
             </Text>
             <Carousel className="Intro__carousel" autoplay dotPosition='bottom'>
                 <div class="Intro__item">
-                    <h3>1</h3>
+                    <Text strong>Reduce Panic</Text>
+                    <Paragraph>By giving public information about supply chain health</Paragraph>
                 </div>
                 <div class="Intro__item">
-                    <h3>2</h3>
+                    <Text strong>Crowdsource Essential Availability</Text>
+                    <Paragraph>Make it easier to acquire essentials for the entire community because the only way we get through this is together.</Paragraph>
                 </div>
                 <div class="Intro__item">
-                    <h3>3</h3>
+                    <Text strong>Identity Disruptions Early</Text>
+                    <Paragraph>Early before they cascade into larger problems</Paragraph>
                 </div>
                 <div class="Intro__item">
-                    <h3>4</h3>
+                    <Text strong>Provide Visibility</Text>
+                    <Paragraph>For governments and agencies to better identify and address supply chain disruptions</Paragraph>
                 </div>
             </Carousel>
         </section>
