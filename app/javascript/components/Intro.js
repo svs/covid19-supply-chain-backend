42c08b0d84d0789511bf2f0c4665ae038e38a087
Swapnil Singh <mail@swapnilsingh.me>
2020-04-03 15:55:39 +0530

    Adds icons

--- app/javascript/components/Intro.js
+++ app/javascript/components/Intro.js
@@ -1,9 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Carousel, Space, Typography } from 'antd'
+import { PanicIcon } from './Icons'
 import './Intro.css';
 
 const { Text, Paragraph } = Typography;
+
+const iconStyles = {
+    width: '128px',
+    height: '128px',
+}
 
 const Intro = () => {
     return (
@@ -16,18 +22,22 @@ const Intro = () => {
             </Text>
             <Carousel className="Intro__carousel" autoplay dotPosition='bottom'>
                 <div class="Intro__item">
+                    <PanicIcon style={iconStyles} /><br />
                     <Text strong>Reduce Panic</Text>
                     <Paragraph>By giving public information about supply chain health</Paragraph>
                 </div>
                 <div class="Intro__item">
+                    <PanicIcon style={iconStyles} /><br />
                     <Text strong>Crowdsource Essential Availability</Text>
                     <Paragraph>Make it easier to acquire essentials for the entire community because the only way we get through this is together.</Paragraph>
                 </div>
                 <div class="Intro__item">
+                    <PanicIcon style={iconStyles} /><br />
                     <Text strong>Identity Disruptions Early</Text>
                     <Paragraph>Early before they cascade into larger problems</Paragraph>
                 </div>
                 <div class="Intro__item">
+                    <PanicIcon style={iconStyles} /><br />
                     <Text strong>Provide Visibility</Text>
                     <Paragraph>For governments and agencies to better identify and address supply chain disruptions</Paragraph>
                 </div>
