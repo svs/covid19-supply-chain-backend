01e9b82661df38e41b0a4be25f3a7fb3135f843e
Anubhav Jain <anubhavj1357@gmail.com>
2020-04-05 03:07:32 +0530

    Adds route to upload photos and return a response

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
+
+  namespace :api do
+    namespace :v1 do
+      post 'photos', to: 'photos#create'
+    end
+  end
   post '/api/availability_reports', to: 'availability_reports#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
