4273ed891bc6c17b4f33abb797c6233377ddd4ca
Swapnil Singh <mail@swapnilsingh.me>
2020-04-16 00:05:17 +0530

    Changes param to match antd standard

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PhotosController < ApiController
       def create
-        photo = Photo.create!(image: photo_params[:image])
+        photo = Photo.create!(image: photo_params[:file])
         if photo
           render json: { success: true, photo_id: photo.id, photo_url: photo.image_url }
         else
@@ -13,7 +13,7 @@ module Api
       private
 
       def photo_params
-        params.permit(:image)
+        params.permit(:file)
       end
     end
   end
