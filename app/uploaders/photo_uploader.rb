a7fdc591baa4869f4477eb30e9be42127a75826a
Anubhav Jain <anubhavj1357@gmail.com>
2020-04-26 10:35:19 +0530

    Indentation fix

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,5 @@
 class PhotoUploader < Shrine
   plugin :instrumentation
-  plugin :determine_mime_type, analyzer: :marcel, analyzer_options: { filename_fallback: true }
+  plugin :determine_mime_type, analyzer: :marcel,
+                                                analyzer_options: { filename_fallback: true }
 end
